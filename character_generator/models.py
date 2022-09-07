b8e472595fa7c433ca2ee092100b36f4051c72e8
Bernie Lawson <lawsonbh@gmail.com>
2022-09-06 22:31:08 -0400

    Setting up placeholder models for char gen

--- character_generator/models.py
+++ character_generator/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    ##TODO: A user has many egos
+    pass
+
+class Ego(models.Model):
+    ##TODO: An Ego has many morphs and many items
+    pass
+
+class Morph(models.Model):
+    ##TODO: A morph has many egos
+    pass
+
+class Item(models.Model):
+    ##TODO: An item has many egos
