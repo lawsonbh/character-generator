a5ec2243b0f47f134a3c35cea8f0f38a322d5a62
Bernie Lawson <lawsonbh@gmail.com>
2022-09-11 23:03:29 -0400

    Working towards form view

--- bodybank/character_generator/urls.py
+++ bodybank/character_generator/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.index, name='index')
+        path('', views.index, name='index'),
+        path('ego/',views.get_ego,name='build-ego')
 ]
