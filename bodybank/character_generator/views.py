4b034c212ca67611542ceb975f2ba77af16a9460
Bernie Lawson <lawsonbh@gmail.com>
2022-09-21 22:17:47 -0400

    Mixins make much more sense now

--- bodybank/character_generator/views.py
+++ bodybank/character_generator/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.views.generic.edit import UpdateView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .forms import EgoForm
 from .models import Ego
 from django.contrib.auth.decorators import login_required
@@ -26,12 +28,34 @@ def get_ego(request):
 def say_thanks(request):
     return render(request, 'thanks.html')
 
-class EgoUpdateView(UpdateView):
-    model = Ego
-    fields = ['cog','inte','ref','sav','som','wil','morph','items','user']
-    template_name_suffix = '_update_form'
-
+class OwnerMixin():
     def get_queryset(self):
-        # Override default query set to return only active user
+        #Override default query set to return only active user
         queryset = super().get_queryset()
         return queryset.filter(owner=self.request.user)
+
+class OwnerEditMixin():
+    #because Antonio's book told me to : )
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class OwnerEgoMixin(OwnerMixin):
+    model = Ego
+    fields = ['cog','inte','ref','sav','som','wil','morph','items','user']
+    success_url = reverse_lazy('manage_egos_list')
+
+class OwnerEgoEditMixin(OwnerEgoMixin, OwnerEditMixin):
+    template_name = 'character_generator/templates/form.html'
+
+class ManageEgoListView(OwnerEgoMixin, ListView):
+    template_name = 'character_generator/templates/list.html'
+
+class EgoCreateView(OwnerEgoEditMixin, CreateView):
+    pass
+
+class EgoUpdateView(OwnerEgoEditMixin, UpdateView):
+    pass
+
+class EgoDeleteView(OwnerEgoMixin, DeleteView):
+    tempalte_name = 'character_generator/templates/delete.html'
