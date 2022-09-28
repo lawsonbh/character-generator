1ec2ab1b9e5ce63b19d609302e2f7467512aede7
Bernie Lawson <lawsonbh@gmail.com>
2022-09-27 21:36:14 -0400

    Return name on morph model

--- bodybank/character_generator/models.py
+++ bodybank/character_generator/models.py
@@ -18,20 +18,24 @@ class Ego(models.Model):
         return self.name
 
 class Morph(models.Model):
-    name = models.CharField(max_length=50)
-    mtype = models.CharField("Morph Type",max_length=50)
-    cost = models.IntegerField()
-    avail = models.IntegerField("Availability")
-    wt = models.IntegerField("Wound Threshold")
-    dur = models.IntegerField("Durability")
-    dr = models.IntegerField("Death Rating")
-    ins = models.IntegerField("Insight")
-    mox = models.IntegerField("Moxie")
-    vig = models.IntegerField("Vigor")
-    flex = models.IntegerField()
-    movtype = models.CharField("Movement System",max_length=20)
-    mov_base = models.IntegerField("Movement Base Rate")
-    mov_full = models.IntegerField("Movement Full Rate")
+    name = models.CharField(max_length=50,null=True)
+    desc = models.TextField("Morph Description",default="")
+    mtype = models.CharField("Morph Type",max_length=50,default="")
+    cost = models.IntegerField(default=0)
+    avail = models.IntegerField("Availability",default=0)
+    wt = models.IntegerField("Wound Threshold",default=0)
+    dur = models.IntegerField("Durability",default=0)
+    dr = models.IntegerField("Death Rating",default=0)
+    ins = models.IntegerField("Insight",default=0)
+    mox = models.IntegerField("Moxie",default=0)
+    vig = models.IntegerField("Vigor",default=0)
+    flex = models.IntegerField(default=0)
+    movtype = models.CharField("Movement System",max_length=20,default="Walker")
+    mov_base = models.IntegerField("Movement Base Rate",default=4)
+    mov_full = models.IntegerField("Movement Full Rate",default=20)
+
+    def __str__(self):
+        return self.name
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
