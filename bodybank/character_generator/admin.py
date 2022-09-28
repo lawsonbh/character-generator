3e5a77e6777d1f9978ddeba114afce86964fd6fc
Bernie Lawson <lawsonbh@gmail.com>
2022-09-27 21:47:43 -0400

    Set up displaying list of morphs added via admin

--- bodybank/character_generator/admin.py
+++ bodybank/character_generator/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Ego
+from .models import Ego, Morph
 
 
 class EgoAdmin(admin.ModelAdmin):
     pass
 
+class MorphAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Ego, EgoAdmin)
+admin.site.register(Morph, MorphAdmin)
